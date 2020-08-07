7d49a6de90d6f0509eac8a4d6da5c68ae8ce79c9
joaomsv <joaomarcossv@gmail.com>
2020-08-07 00:24:42 -0300

    Finalized api tests

--- cypress/integration/api_tests.feature
+++ cypress/integration/api_tests.feature
@@ -2,3 +2,17 @@ Feature: API Tests
 
     API tests for the ciandt challenge.
     Host: https://jsonplaceholder.typicode.com/
+
+    Scenario: Get users
+        When I request a 'GET' from the host 'https://jsonplaceholder.typicode.com/users' with the body ''
+        Then all users should have a name, username, and email
+        And their Email must be valid
+        And their Company catchphrase must have less than 50 characters
+
+    Scenario: Post posts with user id from get
+        When I request a POST with GET user id
+        Then the post should be saved successfully
+
+    Scenario: Post posts without title
+        When I request a 'POST' from the host 'https://jsonplaceholder.typicode.com/posts' with the body '{ userId: 1, body: "test body" }'
+        Then the post should not be saved successfully
---
+++ cypress/integration/api_tests/api_tests.js
@@ -0,0 +1,101 @@
+/// <reference types="cypress" />
+
+import { Given, When, Then, Before } from 'cypress-cucumber-preprocessor/steps'
+
+// describe('api tests', () => {
+//   // Cypress.config('baseUrl', 'https://jsonplaceholder.typicode.com')
+
+//   it.skip('GET api test', () => {
+//     function validateEmail(email) {
+//       const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//       return re.test(email)
+//     }
+//     cy.request({ method: 'GET', url: 'https://jsonplaceholder.typicode.com/users/' }).then((response) => {
+//       response.body.forEach((element) => {
+//         expect(element.name).not.empty
+//         expect(element.username).not.empty
+//         expect(element.email).not.empty
+//         // cy.validateEmail(element.email).should('be.true')
+//         expect(validateEmail(element.email)).to.be.true
+//         expect(element.company.catchPhrase).to.be.length.of.below(50)
+//       })
+//     })
+//   })
+
+//   it('POST api test', () => {
+//     cy.request('GET', 'https://jsonplaceholder.typicode.com/users').then((response) => {
+//       response.body.forEach((user) => {
+//         cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', {
+//           userId: user.id,
+//           title: 'test title',
+//           body: 'test body'
+//         }).then((response) => {
+//           expect(response.status).to.be.eq(201)
+//         })
+//       })
+//     })
+//     cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', { userId: 1, body: 'test body' }).then(
+//       (response) => {
+//         expect(response.status).to.be.eq(400)
+//       }
+//     )
+//   })
+// })
+
+When('I request a {string} from the host {string} with the body {string}', (Method, Host, Body) => {
+  cy.request(Method, Host, Body).as('api')
+})
+
+Then('all users should have a name, username, and email', () => {
+  cy.get('@api').then((response) => {
+    response.body.forEach((element) => {
+      expect(element.name).not.empty
+      expect(element.username).not.empty
+      expect(element.email).not.empty
+    })
+  })
+})
+
+Then('their Email must be valid', () => {
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email)
+  }
+  cy.get('@api').then((response) => {
+    response.body.forEach((element) => {
+      expect(validateEmail(element.email)).to.be.true
+    })
+  })
+})
+
+Then('their Company catchphrase must have less than 50 characters', () => {
+  cy.get('@api').then((response) => {
+    response.body.forEach((element) => {
+      expect(element.company.catchPhrase).to.be.length.of.below(50)
+    })
+  })
+})
+
+When('I request a POST with GET user id', () => {
+  cy.request('GET', 'https://jsonplaceholder.typicode.com/users').as('getId')
+})
+
+Then('the post should be saved successfully', () => {
+  cy.get('@getId').then((response) => {
+    response.body.forEach((user) => {
+      cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', {
+        userId: user.id,
+        title: 'test title',
+        body: 'test body'
+      }).then((response) => {
+        expect(response.status).to.be.eq(201)
+      })
+    })
+  })
+})
+
+Then('the post should not be saved successfully', () => {
+  cy.get('@api').then((response) => {
+    expect(response.status).to.be.eq(400)
+  })
+})
